61ce77943884f0f99253af8e23943caa435429de
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2022-03-11 22:13:01 +0530

    Todo-App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false
+    };
+    setTodos([...todos.concat(newTodo)]);
+    setTodo("");
+  };
+
+  const deleteHandler = (id) => {
+    const newTodo = [...todos].filter((el) => el.id !== id);
+    setTodos(newTodo);
+  };
+
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <h1>Todo-App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((e) => {
+        return (
+          <div key={e.id}>
+            <p>{e.text}</p>
+            <button onClick={() => deleteHandler(e.id)}>Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
